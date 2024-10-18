ceabad2cbe911deb6eaacb89bd8fa95f19622ae3
pham the minh <phamtheminh2409@gmail.com>
2024-10-18 08:19:41 +0700

    Update VSCODECBTOOL.bat

--- VSCODECBTOOL.bat
+++ VSCODECBTOOL.bat
@@ -1,4 +1,4 @@
 @echo off
-call "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildToolsVsDevCmd.bat"
+call "C:/Program Files (x86)/Microsoft Visual Studio/2022/BuildTools/Common7/Tools/VsDevCmd.bat"
 code
 exit
